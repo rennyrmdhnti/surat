aa03814591c0d733aaf1281d30a25043861dcc03
sysoken <sayasoken@gmail.com>
2023-08-09 21:19:39 +0800

    npd

--- surat/nota_pencairan_dana.php
+++ surat/nota_pencairan_dana.php
@@ -48,8 +48,16 @@
                                 <thead>
                                     <tr>
                                         <th>No</th>
-                                        <th>Golongan</th>
-                                        <th>Nama Pangkat</th>
+                                        <th>No NPD</th>
+                                        <th>Sub Kegiatan</th>
+                                        <th>No DPA</th>
+                                        <th>Kode Rekening</th>
+                                        <th>Uraian</th>
+                                        <th>Anggaran</th>
+                                        <th>Pencairan</th>
+                                        <th>Tanggal</th>
+                                        <th>Nama PPTK</th>
+                                        <th></th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -67,22 +75,91 @@
         <div class="modal-dialog modal-lg">
             <div class="modal-content">
                 <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Input Data Golongan</h5>
+                    <h5 class="modal-title" id="exampleModalLabel">Input Data Nota Pencairan Dana</h5>
                     <button type="button" class="btn-close" data-coreui-dismiss="modal" aria-label="Close"></button>
                 </div>
                 <div class="modal-body">
                     <div class="mb-3">
-                        <label for="exampleFormControlInput1" class="form-label">Golongan</label>
-                        <input type="text" class="form-control" id="golongan" name="golongan">
+                        <label for="exampleFormControlInput1" class="form-label">No. SPPD</label>
+                        <select class="form-select" id="no_npd" name="no_npd" aria-label="Default select example">
+                            <option value="">-- Pilih No NPD --</option>
+                            <?php
+                                        // Query untuk mengambil data dari tabel tb_perintah_tugas
+                                        $query = "SELECT no_npd FROM tb_nominatif GROUP BY no_npd";
+                                        $result = mysqli_query($conn, $query);
+
+                                        // Iterasi hasil query dan menampilkan nilai kolom "no_spt" dalam elemen select
+                                        while ($row = mysqli_fetch_assoc($result)) {
+                                            echo "<option value='" . $row['no_npd'] . "'>" . $row['no_npd'] . "</option>";
+                                        }
+                                        ?>
+                        </select>
                     </div>
                     <div class="mb-3">
-                        <label for="exampleFormControlInput1" class="form-label">Nama Pangkat</label>
-                        <input type="text" class="form-control" id="nama_pangkat" name="nama_pangkat">
+                        <label for="exampleFormControlInput1" class="form-label">Sub Kegiatan</label>
+                        <select class="form-select" id="sub_kegiatan" name="sub_kegiatan" aria-label="Default select example">
+                            <option value="">-- Pilih Kegiatan --</option>
+                            <?php
+                                        // Query untuk mengambil data dari tabel tb_perintah_tugas
+                                        $query = "SELECT id,sub_kegiatan FROM tb_pagu";
+                                        $result = mysqli_query($conn, $query);
+
+                                        // Iterasi hasil query dan menampilkan nilai kolom "no_spt" dalam elemen select
+                                        while ($row = mysqli_fetch_assoc($result)) {
+                                            echo "<option value='" . $row['id'] . "'>" . $row['sub_kegiatan'] . "</option>";
+                                        }
+                                        ?>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">No DPA</label>
+                        <input type="text" class="form-control" id="no_dpa" name="no_dpa">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Kode Rekening</label>
+                        <select class="form-select" id="kode_rek" name="kode_rek" aria-label="Default select example">
+                            <option value="">-- Pilih Kode Rekening --</option>
+                            <?php
+                                        // Query untuk mengambil data dari tabel tb_perintah_tugas
+                                        $query = "SELECT id_rek,kode_rekening FROM tb_rek_kegiatan";
+                                        $result = mysqli_query($conn, $query);
+
+                                        // Iterasi hasil query dan menampilkan nilai kolom "no_spt" dalam elemen select
+                                        while ($row = mysqli_fetch_assoc($result)) {
+                                            echo "<option value='" . $row['id_rek'] . "'>" . $row['kode_rekening'] . "</option>";
+                                        }
+                                        ?>
+                        </select>
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Pencairan</label>
+                        <input type="text" class="form-control" id="pencairan" name="pencairan">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Tanggal</label>
+                        <input type="date" class="form-control" id="tanggal_npd" name="tanggal_npd">
+                    </div>
+                    <div class="mb-3">
+                        <label for="exampleFormControlInput1" class="form-label">Nama PPTK</label>
+                        <select class="form-select" id="nama_pptk" name="nama_pptk" aria-label="Default select example">
+                            <option value="">-- Pilih Nama PPTK --</option>
+                            <?php
+                                        // Query untuk mengambil data dari tabel tb_perintah_tugas
+                                        $query = "SELECT id,nama FROM tb_pegawai";
+                                        $result = mysqli_query($conn, $query);
+
+                                        // Iterasi hasil query dan menampilkan nilai kolom "no_spt" dalam elemen select
+                                        while ($row = mysqli_fetch_assoc($result)) {
+                                            echo "<option value='" . $row['id'] . "'>" . $row['nama'] . "</option>";
+                                        }
+                                        ?>
+                        </select>
                     </div>
                 </div>
                 <div class="modal-footer">
                     <button type="button" class="btn btn-secondary" data-coreui-dismiss="modal">Close</button>
-                    <button type="button" class="btn btn-primary">Save changes</button>
+                    <button type="button" class="btn btn-primary" onclick="simpanData()">Save
+                        changes</button>
                 </div>
             </div>
         </div>
@@ -95,17 +172,38 @@
             // "processing": true,
             // "serverSide": true,
             "ajax": {
-                "url": "get_data_master.php?data=golongan",
+                "url": "get_data_surat.php?data=npd",
                 "type": "POST"
             },
             "columns": [{
                     "data": ""
                 },
                 {
-                    "data": "kd_golongan"
+                    "data": "no_npd"
                 },
                 {
-                    "data": "nama_pangkat"
+                    "data": "sub_kegiatan"
+                },
+                {
+                    "data": "no_dpa"
+                },
+                {
+                    "data": "kode_rekening"
+                },
+                {
+                    "data": "uraian"
+                },
+                {
+                    "data": "anggaran"
+                },
+                {
+                    "data": "pencairan"
+                },
+                {
+                    "data": "tanggal_npd"
+                },
+                {
+                    "data": "nama"
                 }
             ],
             "columnDefs": [{
@@ -121,15 +219,68 @@
                     // add Edit button
                     $(row).append(
                         '<td><button class="btn btn-primary"><i class="cil-pencil"></i></button></td>'
-                        );
+                    );
                     // add Delete button
                     $(row).append(
                         '<td><button class="btn btn-danger"><i class="cil-trash"></i></button></td>'
-                        );
+                    );
                 }
             }
         });
     });
+
+    function simpanData() {
+        var nonpd = $('#no_npd').val();
+        var subKegiatan = $('#sub_kegiatan').val();
+        var nodpa = $('#no_dpa').val();
+        var kodeRek = $('#kode_rek').val();
+        var pencairan = $('#pencairan').val();
+        var tangalNpd = $('#tanggal_npd').val();
+        var namaPptk = $('#nama_pptk').val();
+
+        $.ajax({
+            url: 'nota_pencairan_dana_simpan.php',
+            method: 'POST',
+            data: {
+                no_npd: nonpd,
+                sub_kegiatan: subKegiatan,
+                no_dpa: nodpa,
+                kode_rek: kodeRek,
+                pencairan: pencairan,
+                tanggal_npd: tangalNpd,
+                nama_pptk: namaPptk,
+            },
+            success: function(response) {
+                // Handle response dari server setelah data disimpan
+                Swal.fire({
+                    icon: 'success',
+                    title: 'Sukses',
+                    text: response,
+                    confirmButtonText: 'OK',
+                    showLoaderOnConfirm: true,
+                    preConfirm: () => {
+                        return new Promise((resolve) => {
+                            // Mengatur waktu delay sebelum mengarahkan ke halaman 'perintah_tugas.php'
+                            setTimeout(() => {
+                                resolve();
+                            }, 3000);
+                        });
+                    }
+                }).then((result) => {
+                    if (result.isConfirmed) {
+                        // Pengguna mengklik tombol "OK"
+                        window.location.href = 'nota_pencairan_dana.php';
+                    }
+                });
+            },
+            error: function(xhr, status, error) {
+                // Handle error jika terjadi masalah saat mengirimkan data
+                console.error(xhr.responseText);
+                alert("Terjadi kesalahan saat menyimpan data. Silakan coba lagi.");
+            }
+        });
+
+    }
     </script>
 
     <?php include '../template/footer.php'; ?>
